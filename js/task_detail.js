f6c5da12def67a9fe63ba67f398495b703f450d6
Christian Manalansan <cmanalansan@gmail.com>
2020-04-25 11:38:19 -0400

    Add initial debounce

--- js/task_detail.js
+++ js/task_detail.js
@@ -1,3 +1,11 @@
-$(function() {
+var task;
 
+$(function() {
+    const updateName = debounce(
+        function() {
+            task.name = $(this).val();
+        },
+        250
+    );
+    $('#task-name').on('keyup', updateName);
 });
