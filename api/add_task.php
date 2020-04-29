1426eb5072c022f20100cc2a152c77c501d28232
Christian Manalansan <cmanalansan@gmail.com>
2020-04-28 22:33:47 -0400

    Rewrite add_task for MySQL

--- api/add_task.php
+++ api/add_task.php
@@ -1,33 +1,20 @@
 <?php
+    require __DIR__ . '/../vendor/autoload.php';
     require_once "AppDB.class.php";
     $data = file_get_contents("php://input");
     $_POST = json_decode($data, TRUE); // not typical, i know
     
-    $db = new AppDB();
-    $sql = "
-        INSERT INTO tasks
-        (task_name, date_created)
-        VALUES
-        (:task_name, :date_created)
-    ";
-    $stmt = $db->prepare($sql);
+    DB::insert(
+        'tasks',
+        [
+            'task_name' => $_POST['name'],
+            'date_created' => new DateTime(),
+        ]
+    );
 
-    // passing values to the parameters
-    $stmt->bindValue(':task_name', $_POST['name']);
-    $stmt->bindValue(':date_created', time());
-
-    $ret = $stmt->execute();
-
-    $sql = "SELECT last_insert_rowid() AS id";
-    $stmt = $db->prepare($sql);
-    $ret = $stmt->execute();
-
-    while ($row = $ret->fetchArray(SQLITE3_ASSOC)) {
-        $out = [
-            'id' => $row['id'],
-            'tempId' => $_POST['tempId'],
-        ];
-        break;
-    }
+    $out = [
+        'id' => DB::insertId(),
+        'tempId' => $_POST['tempId'],
+    ];
 
     exit(json_encode($out));
