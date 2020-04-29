14ba18a84c19f121b3f9be760500d5cc008eb884
Christian Manalansan <cmanalansan@gmail.com>
2020-04-28 22:38:24 -0400

    Rewrite load_task for MySQL

--- api/load_task.php
+++ api/load_task.php
@@ -1,36 +1,31 @@
 <?php
+    require __DIR__ . '/../vendor/autoload.php';
     require_once "AppDB.class.php";
 
-    $db = new AppDB();
-    $sql = "
-        SELECT t.task_id, t.task_name, t.date_due, td.task_note, td.subtask_set_order
-        FROM tasks t
-        LEFT OUTER JOIN task_details td ON td.task_Id = t.task_id
-        WHERE t.task_id = :task_id
-    ";
+    $row = DB::queryFirstRow(
+        "
+            SELECT t.task_id, t.task_name, t.date_due, td.task_note, td.subtask_set_order
+            FROM tasks t
+            LEFT OUTER JOIN task_details td ON td.task_Id = t.task_id
+            WHERE t.task_id = %i
+        ",
+        $_GET['taskId']
+    );
 
-    $stmt = $db->prepare($sql);
-    $stmt->bindValue(':task_id', $_GET['taskId']);
-    $ret = $stmt->execute();
-
-    $out = false;
-    while ($row = $ret->fetchArray(SQLITE3_ASSOC)) {
-        $formatted_date = false;
-        if ($row['date_due']) {
-            // e.g.: Fri, Apr 4
-            $formatted_date = date("Y-m-d", $row['date_due']);
-        }
-
-        $out = [
-            'id' => $row['task_id'],
-            'name' => $row['task_name'],
-            'dateDue' => $formatted_date,
-            'taskNote' => $row['task_note'],
-            'subtaskSetOrder' => $row['subtask_set_order'] ?? FALSE,
-        ];
-
-        // we are only expecting one result 
-        break;
+    if (empty($row)) {
+        exit(json_encode(FALSE));
     }
 
-    exit(json_encode($out));
+    $formatted_date = false;
+    if ($row['date_due']) {
+        // e.g.: Fri, Apr 4
+        $formatted_date = date("Y-m-d", $row['date_due']);
+    }
+
+    exit(json_encode([
+        'id' => $row['task_id'],
+        'name' => $row['task_name'],
+        'dateDue' => $formatted_date,
+        'taskNote' => $row['task_note'],
+        'subtaskSetOrder' => $row['subtask_set_order'] ?? FALSE,
+    ]));
