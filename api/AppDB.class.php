3aa0e0177e8e8486fda2ab4ccc7fb879664d8950
Christian Manalansan <cmanalansan@gmail.com>
2020-04-29 06:29:30 -0400

    Use JawsDB

--- api/AppDB.class.php
+++ api/AppDB.class.php
@@ -1,6 +1,12 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-DB::$user = 'user';
-DB::$password = 'password';
-DB::$dbName = 'ftd';
+// @see https://devcenter.heroku.com/articles/jawsdb#using-jawsdb-with-php
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+$database = ltrim($dbparts['path'],'/');
+
+DB::$hostname = $dbparts['host'] ?? 'localhost';
+DB::$user = $dbparts['user'] ?? 'user';
+DB::$password = $dbparts['pass'] ?? 'password';
+DB::$dbName = $database ?? 'ftd';
