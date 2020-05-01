9be7c7331f3705c52997c7aa8d030e64a56db190
Christian Manalansan <cmanalansan@gmail.com>
2020-05-01 16:24:56 -0400

    Exclude completed items from load_task_list

--- api/load_task_list.php
+++ api/load_task_list.php
@@ -3,33 +3,57 @@
     require_once "AppDB.class.php";
 
     $results = DB::query("
-        SELECT t.task_id, t.task_name, t.date_due, t.is_more, t.respawn, td.task_note, s.subtask_name
+        SELECT t.task_id, t.task_name, t.date_due, t.respawn
         FROM tasks t
-        LEFT OUTER JOIN task_details td ON t.task_id = td.task_id 
-        LEFT OUTER JOIN subtasks s ON t.task_id = s.task_id 
         WHERE t.date_completed IS NULL
         ORDER BY t.date_created DESC
     ");
 
-    exit(json_encode(array_map(
-        function($row) {
+    $is_more_map = build_map($results);
+    $out = array_map(
+        function($row) use ($is_more_map) {
             return [
                 'id' => $row['task_id'],
                 'name' => $row['task_name'],
                 'dateTimestamp' => $row['date_due'] ?? FALSE,
                 'dateDue' => get_formatted_date($row['date_due']),
-                'isMore' => has_extra_details($row),
+                'isMore' => $is_more_map[$row['task_id']],
                 'respawn' => $row['respawn'] ?? FALSE,
             ];
         },
         $results
-    )));
+    );
+
+    exit(json_encode($out));
+
+    /**
+     * Build a mapping array to determine which tasks have notes/subtasks, and which ones do not.
+     */
+    function build_map($results) {
+        $extra_results = DB::query(
+            "
+                SELECT t.task_id, td.task_note, s.subtask_name
+                FROM tasks t
+                LEFT OUTER JOIN task_details td ON t.task_id = td.task_id 
+                LEFT OUTER JOIN subtasks s ON t.task_id = s.task_id
+                WHERE t.task_id IN %li
+            ",
+            array_map(function($row) { return $row['task_id']; }, $results)
+        );
+
+        $is_more_map = [];
+        foreach ($extra_results as $row) {
+            $is_more_map[$row['task_id']] = has_extra_details($row);
+        }
+
+        return $is_more_map;
+    }
 
     function get_formatted_date($date) {
         if (empty($date)) { return FALSE; }
         
         // e.g.: Fri, Apr 4
-        return date("D, M j", strtotime($row['date_due']));
+        return date("D, M j", strtotime($date));
     }
 
     function has_extra_details($row) {
